a29ea2c314d8c9703367cb8b1db8dff02c94ed06
Vincent LAURENT <vince.laurent@gmail.com>
2024-04-24 00:49:41 +0200

    Script pour retirer les bus et les perturbation du futur dans un fichier json retiré depuis l'api de récupération des "disruptions"

---
+++ bin/optimize_disruption_file.php
@@ -0,0 +1,72 @@
+<?php
+
+date_default_timezone_set('Europe/Paris');
+
+$jsonFile = $argv[1];
+
+$json = json_decode(file_get_contents($jsonFile));
+$jsonOriginal = json_decode(file_get_contents($jsonFile));
+
+$dateFile = new DateTime($json->lastUpdatedDate, new DateTimeZone("UTC"));
+$dateFile->setTimeZone(new DateTimeZone(date_default_timezone_get()));
+
+$disruptionIdsDeleted = [];
+
+foreach($json->lines as $indexLine => $line) {
+    if($line->mode != "Bus") {
+        continue;
+    }
+
+    foreach($line->impactedObjects as $indexObject => $object) {
+        $disruptionIdsDeleted = array_merge($disruptionIdsDeleted, $object->disruptionIds);
+    }
+    unset($json->lines[$indexLine]);
+}
+
+$disruptionIdsDeleted = array_unique($disruptionIdsDeleted);
+
+foreach($json->disruptions as $indexDisruption => $disruption) {
+    if(in_array($disruption->id, $disruptionIdsDeleted)) {
+        unset($json->disruptions[$indexDisruption]);
+        continue;
+    }
+    foreach($disruption->applicationPeriods as $indexPeriod => $period) {
+        $dateBegin = new DateTime($period->begin);
+        $dateEnd = new DateTime($period->end);
+        if($dateFile > $dateBegin && $dateFile < $dateEnd) {
+            continue;
+        }
+        unset($disruption->applicationPeriods[$indexPeriod]);
+    }
+    $disruption->applicationPeriods = array_values($disruption->applicationPeriods);
+    if(!count($disruption->applicationPeriods)) {
+        $disruptionIdsDeleted[] = $disruption->id;
+        unset($json->disruptions[$indexDisruption]);
+        continue;
+    }
+}
+
+$json->disruptions = array_values($json->disruptions);
+
+foreach($json->lines as $indexLine => $line) {
+    foreach($line->impactedObjects as $indexObject => $object) {
+        foreach($object->disruptionIds as $indexDisruptionId => $diruptionId) {
+            if(in_array($diruptionId, $disruptionIdsDeleted)) {
+                unset($object->disruptionIds[$indexDisruptionId]);
+            }
+        }
+        $object->disruptionIds = array_values($object->disruptionIds);
+        if(!count($object->disruptionIds)) {
+            $disruptionIdsDeleted[] = $disruption->id;
+            unset($line->impactedObjects[$indexObject]);
+        }
+    }
+    $line->impactedObjects = array_values($line->impactedObjects);
+    if(!count($line->impactedObjects)) {
+        unset($json->lines[$indexLine]);
+    }
+}
+
+$json->lines = array_values($json->lines);
+
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
