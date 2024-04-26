1f1f8d1c855c0bb12883468bf039e389cbbb700b
Vincent LAURENT <vince.laurent@gmail.com>
2024-04-26 15:55:19 +0200

    Lien symbolique vers les pages de chaque mode de transport

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-currentdate=$(date +%Y%m%d --date="-3 hours")
+todaydate=$(date +%Y%m%d --date="-3 hours")
+currentdate=$todaydate
 
 if test $1; then
     currentdate=$1
@@ -14,7 +15,10 @@ php index.php $currentdate metros > static/$currentdate/metros.html
 php index.php $currentdate trains > static/$currentdate/trains.html
 php index.php $currentdate tramways > static/$currentdate/tramways.html
 
-ln -fs $(date +%Y%m%d --date="-3 hours")/metros.html static/index.html
+ln -fs $todaydate/metros.html static/metros.html
+ln -fs $todaydate/tramways.html static/tramways.html
+ln -fs $todaydate/trains.html static/trains.html
+ln -fs metros.html static/index.html
 
 git add static/index.html
 git add static/$currentdate
