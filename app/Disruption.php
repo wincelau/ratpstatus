d964ea9ff92abfc56fa5d67b5192b8945e01997e
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-05 13:40:18 +0200

    Exclusion

--- app/Disruption.php
+++ app/Disruption.php
@@ -69,6 +69,10 @@ class Disruption
             return true;
         }
 
+        if(preg_match('/Adaptation/', $this->getTitle())) {
+            return true;
+        }
+
         if($this->getCause() == self::CAUSE_TRAVAUX && $this->getSeverity() == self::SEVERITY_PERTURBEE && preg_match('/Ligne D/', $this->getTitle())) {
             return true;
         }
