ec9af3851b940dba89f1a1ff0a000abbb8812604
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-08 01:46:44 +0200

    Nettoyage du fichier

--- bin/fetchapi_lines_file.sh
+++ bin/fetchapi_lines_file.sh
@@ -2,4 +2,4 @@ APIKEY=$1
 
 file=datas/jsonlines/$(date +%Y%m%d%H%M%S)_lines.json
 
-curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/lines?count=100&forbidden_id%5B%5D=physical_mode%3ABus&disable_geojson=true&disable_disruption=true" -H 'accept: application/json' > $file
+curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/lines?count=100&forbidden_id%5B%5D=physical_mode%3ABus&disable_geojson=true&disable_disruption=true" -H 'accept: application/json' | jq -c 'del(.lines[].routes) | del(.links) | del(.pagination) | del(.feed_publishers) | del(.disruptions)' > $file
