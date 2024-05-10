f45c662eb3352905c4d05b4e8e0666b3e4b0afb5
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-11 01:05:37 +0200

    Pas besoin de jq

--- bin/fetchapi_traffic_reports_file.sh
+++ bin/fetchapi_traffic_reports_file.sh
@@ -4,4 +4,4 @@ mkdir datas/jsontrafficreports
 
 file=datas/jsontrafficreports/$(date +%Y%m%d%H%M%S)_traffic_reports.json
 
-curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/traffic_reports?count=100" -H 'accept: application/json' | jq > $file
+curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/traffic_reports?count=100" -H 'accept: application/json' > $file
