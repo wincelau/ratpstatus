f656473f0cc1ec133bd5a5cd463377bd88c6a965
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-11 01:08:55 +0200

    gzip à la fin

--- bin/fetchapi_line_reports_file.sh
+++ bin/fetchapi_line_reports_file.sh
@@ -7,3 +7,5 @@ curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/
 curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/line_reports/physical_modes%2Fphysical_mode%3ALocalTrain/line_reports?count=100" -H 'accept: application/json' > $file.transilien.json
 curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/line_reports/physical_modes%2Fphysical_mode%3ARapidTransit/line_reports?count=100" -H 'accept: application/json' > $file.rer.json
 curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/line_reports/physical_modes%2Fphysical_mode%3ATramway/line_reports?count=100" -H 'accept: application/json' > $file.tramway.json
+
+gzip datas/jsonlinereports/*.json &
--- bin/fetchapi_traffic_reports_file.sh
+++ bin/fetchapi_traffic_reports_file.sh
@@ -5,3 +5,5 @@ mkdir datas/jsontrafficreports
 file=datas/jsontrafficreports/$(date +%Y%m%d%H%M%S)_traffic_reports.json
 
 curl -H "apiKey: $APIKEY" "https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/traffic_reports?count=100" -H 'accept: application/json' > $file
+
+gzip datas/jsontrafficreports/*.json &
