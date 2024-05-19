49838f5793504ad57df96707643f37caf2e117c8
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-19 11:34:07 +0200

    Stock les fichiers récupérés dans un dossier

--- bin/fetchapi_disruption_file.sh
+++ bin/fetchapi_disruption_file.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 APIKEY=$1
 
+todaydate=$(date +%Y%m%d --date="-3 hours")
 filename=$(date +%Y%m%d%H%M%S)_disruptions
 filebrut=datas/jsonbrut/$filename.json
-fileoptimized=datas/json/$filename.optimized.json
+fileoptimized=datas/json/$todaydate/"$filename".optimized.json
+mkdir -p datas/json/$todaydate
+
 curl -H "apiKey: $APIKEY" https://prim.iledefrance-mobilites.fr/marketplace/disruptions_bulk/disruptions/v2 > $filebrut
 php bin/optimize_disruption_file.php $filebrut > $fileoptimized
 git pull
