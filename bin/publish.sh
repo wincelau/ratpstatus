ed11acd3114d402024d238f59fce0a396c337bd5
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-21 18:20:34 +0200

    Fichier temporaire pour éviter de tout cassé

--- bin/publish.sh
+++ bin/publish.sh
@@ -24,7 +24,11 @@ php index.php $currentdate tramways > static/$currentdate/tramways.html.tmp
 php json.php $currentdate > static/$currentdate.json.tmp
 php csv.php $currentdate > static/$currentdate.csv.tmp
 
-mv static/$currentdate/*{.tmp,}
+mv static/$currentdate/metros.html{.tmp,}
+mv static/$currentdate/trains.html{.tmp,}
+mv static/$currentdate/tramways.html{.tmp,}
+mv static/$currentdate.csv{.tmp,}
+mv static/$currentdate.json{.tmp,}
 
 ln -fs $todaydate/metros.html static/metros.html
 ln -fs $todaydate/tramways.html static/tramways.html
