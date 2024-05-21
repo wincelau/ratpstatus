04e9ab8c8b5cd1308c6888a950cb11df1c26821d
Vincent LAURENT <vince.laurent@gmail.com>
2024-05-22 00:59:42 +0200

    publish no interaction avec git

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,19 +2,10 @@
 
 todaydate=$(date +%Y%m%d --date="-3 hours")
 currentdate=$todaydate
-nopush=0
 
 if test $1; then
     currentdate=$1
 fi
-
-if test $2; then
-    nopush=$2
-fi
-
-git checkout static/metros.html static/tramways.html static/trains.html static/index.html static/$currentdate static/$currentdate.json static/$currentdate.csv
-
-git pull --commit
 
 mkdir static/$currentdate 2> /dev/null
 
@@ -34,17 +25,3 @@ ln -fs $todaydate/metros.html static/metros.html
 ln -fs $todaydate/tramways.html static/tramways.html
 ln -fs $todaydate/trains.html static/trains.html
 ln -fs metros.html static/index.html
-
-git add static/metros.html
-git add static/tramways.html
-git add static/trains.html
-git add static/index.html
-git add static/$currentdate
-git add static/$currentdate.json
-git add static/$currentdate.csv
-
-git commit static/metros.html static/tramways.html static/trains.html static/index.html static/$currentdate static/$currentdate.json static/$currentdate.csv -m "Publication des pages html statique pour $currentdate"
-
-if test $nopush == 0; then
-    git push
-fi
--- bin/publish_all.sh
+++ bin/publish_all.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 find static/[0-9]* -type d | cut -d "/" -f 2 | while read file; do bash bin/publish.sh $file 1; done
-
-git push
