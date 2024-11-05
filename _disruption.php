9b2ae86418e434792f545bf0ef860149766d8ea6
Vincent LAURENT <vince.laurent@gmail.com>
2024-11-05 23:37:47 +0100

    Liste des incidents avec un titre de page et séparé en incident en cours et terminé

---
+++ _disruption.php
@@ -0,0 +1,23 @@
+<div class="card my-3">
+    <div class="card-header">
+        <h5><img style="height: 20px;" src="<?php echo $disruption->getLigne()->getImage(); ?>" />
+            <span class="badge text-bg-light"><?php if($disruption->getCause() == Impact::CAUSE_TRAVAUX): ?>🚧<?php else: ?>⚠️<?php endif; ?></span>
+        <span class="badge text-bg-light">🕥 <?php if($disruption->getDateEnd() < new DateTime()): ?>De <?php echo $disruption->getDateStart()->format("H\hi") ?> à <?php echo $disruption->getDateEnd()->format("H\hi") ?><?php else: ?>Depuis <?php echo $disruption->getDateStart()->format("H\hi") ?><?php endif; ?></span>
+        <span class="badge text-bg-light float-end">⌛ <?php echo $disruption->getDuration()->format("%hh%I"); ?></span>
+        </h5>
+    </div>
+    <ul class="list-group list-group-flush">
+    <?php $isFirst = true; ?>
+    <?php foreach($disruption->getImpacts() as $i): ?>
+        <?php if($isFirst && $disruption->getDateEnd() < new DateTime()): ?>
+            <li class="list-group-item">
+                <strong class="me-2 small"><?php echo $i->getDateEnd()->format("H\hi") ?></strong> <span class="ok text-white strong float-end rounded px-2 small">Fin</span> <?php echo $i->getTitle() ?>
+            </li>
+            <?php $isFirst = false; ?>
+        <?php endif; ?>
+            <li class="list-group-item" style="overflow: hidden; max-height: 40px;">
+                <strong class="me-2 small"><?php echo $i->getDateStart()->format("H\hi") ?></strong> <span class="<?php echo $i->getColorClass() ?> text-white strong float-end rounded px-2 small"><?php echo $i->getDuration()->format("%hh%I"); ?></span> <?php echo $i->getTitle() ?>
+            </li>
+    <?php endforeach; ?>
+    </ul>
+</div>
--- list.php
+++ list.php
@@ -11,28 +11,18 @@
     <link rel="stylesheet" href="/css/style.css?202405081203">
 </head>
 <body>
-    <div class="container-md" style="max-width: 800px;">
-<?php foreach($day->getDisruptions() as $disruption): ?>
-        <div class="card my-3 me-4">
-            <div class="card-header">
-                <h5><img style="height: 20px;" src="<?php echo $disruption->getLigne()->getImage(); ?>" />
-                    <span class="badge text-bg-light"><?php if($disruption->getCause() == Impact::CAUSE_TRAVAUX): ?>🚧<?php else: ?>⚠️<?php endif; ?></span>
-                <span class="badge text-bg-light">🕥 <?php if($disruption->getDateEnd() < new DateTime()): ?>De <?php echo $disruption->getDateStart()->format("H\hi") ?> à <?php echo $disruption->getDateEnd()->format("H\hi") ?><?php else: ?>Depuis <?php echo $disruption->getDateStart()->format("H\hi") ?><?php endif; ?></span>
-                <span class="badge text-bg-light">⌛ <?php echo $disruption->getDuration()->format("%hh%I"); ?></span>
-                </h5>
-            </div>
-            <ul class="list-group list-group-flush">
-            <?php foreach($disruption->getImpacts() as $i): ?>
-                    <li class="list-group-item">
-                        <span class="badge text-bg-light"><?php echo $i->getDateStart()->format("H\hi") ?></span> <span class="<?php echo $i->getColorClass() ?> text-white strong float-end rounded px-2 small"><?php echo $i->getDuration()->format("%hh%I"); ?></span> <?php echo $i->getTitle() ?>
-                        <!-- <p><?php echo nl2br(html_entity_decode(strip_tags(str_replace(["<br>", "</p>"], "\n", $i->getMessage())))) ?></p> -->
-                        <!-- <small class="text-muted">MAJ <?php echo $i->getLastUpdate()->format('d/m/Y H:i:s') ?></small>
-                        <small class="text-muted"> - FILE <?php echo $i->getDateCreation()->format('d/m/Y H:i:s') ?></small> -->
-                    </li>
-            <?php endforeach; ?>
-            </ul>
-        </div>
-<?php endforeach; ?>
+    <div class="container-sm" style="max-width: 720px;">
+        <h1 class="mt-3">Incident du <?php echo $day->getDateStart()->format('d/m/Y'); ?></h1>
+        <h2 class="mt-3">En cours</h2>
+        <?php foreach($day->getDisruptions() as $disruption): ?>
+                <?php if($disruption->getDateEnd() < new DateTime()): continue; endif; ?>
+                <?php include(__DIR__.'/_disruption.php') ?>
+        <?php endforeach; ?>
+        <h2 class="mt-3">Terminé</h2>
+        <?php foreach($day->getDisruptions() as $disruption): ?>
+            <?php if($disruption->getDateEnd() >= new DateTime()): continue; endif; ?>
+            <?php include(__DIR__.'/_disruption.php') ?>
+        <?php endforeach; ?>
 </div>
 </body>
 </html>
