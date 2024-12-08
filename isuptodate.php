89702295a568aed1f5058db87fe9d3069abbf05a
Vincent LAURENT <vince.laurent@gmail.com>
2024-12-09 00:26:40 +0100

    Script php pour tester que la récupération fonctionne bien

---
+++ isuptodate.php
@@ -0,0 +1,33 @@
+<?php
+
+date_default_timezone_set('Europe/Paris');
+
+require __DIR__.'/app/File.php';
+require __DIR__.'/app/Impact.php';
+
+header('Content-Type: text/plain');
+
+$today = (new DateTime())->modify('-3 minutes');
+
+foreach(scandir(__DIR__.'/datas/json', SCANDIR_SORT_DESCENDING) as $folder) {
+    if(!is_dir(__DIR__.'/datas/json/'.$folder) ){
+        continue;
+    }
+    foreach(scandir(__DIR__.'/datas/json/'.$folder, SCANDIR_SORT_DESCENDING) as $file) {
+        $file = new File(__DIR__.'/datas/json/'.$folder.'/'.$file);
+        if(!$file->isValid()) {
+            http_response_code('500');
+            echo "Le dernier fichier récupéré date d'il y a moins de 2 minutes : ERROR (Fichier non conforme)\n";
+            exit;
+        }
+        if($today > $file->getDate()) {
+            http_response_code('500');
+            echo "Le dernier fichier récupéré date d'il y a moins de 2 minutes : ERROR (".$file->getDate()->format('Y-m-d H:i:s').")\n";
+            exit;
+        }
+        break;
+    }
+    break;
+}
+
+echo "Le dernier fichier récupéré date d'il y a moins de 2 minutes : SUCCESS\n";
