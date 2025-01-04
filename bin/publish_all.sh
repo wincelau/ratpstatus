3f3168768d6ade33a962b5ba3574ddb2aec5527c
Vincent LAURENT <vince.laurent@gmail.com>
2025-01-05 00:06:07 +0100

    Aggrégation des exports des données

--- bin/publish_all.sh
+++ bin/publish_all.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 find static/[0-9]* -type d | cut -d "/" -f 2 | sort -r | while read file; do bash bin/publish.sh $file 1; done
+
+mkdir static/export 2> /dev/null
+
+cat static/*/incidents.csv | head -n 1 > static/export/incidents.csv
+cat static/20*/incidents.csv | grep -v "^date" >> static/export/incidents.csv
+
+cat static/*/timeline.csv | head -n 1 > static/export/chronologie_statuts.csv
+cat static/20*/timeline.csv | grep -v "^date" >> static/export/chronologie_statuts.csv
