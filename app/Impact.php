d663f5d8ee7c3316526121a0d0d53ec8accd4a16
Vincent LAURENT <vince.laurent@gmail.com>
2025-01-17 17:43:18 +0100

    Détéction de la modification de composition plus permissive

--- app/Impact.php
+++ app/Impact.php
@@ -215,7 +215,7 @@ class Impact
             return self::TYPE_AUCUNE;
         }
 
-        if(preg_match('/Modifications de compositions/', $this->getTitle())) {
+        if(preg_match('/Modifications? de compositions?/i', $this->getTitle())) {
             return self::TYPE_AUCUNE;
         }
 
