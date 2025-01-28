002c3fd985c6d6fbc42cef97fb47be23fa8220bb
Vincent LAURENT <vince.laurent@gmail.com>
2025-01-28 08:36:05 +0100

    Publication de la page par mois

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,7 +7,10 @@ if test $1; then
     currentdate=$1
 fi
 
+currentmonth=$(echo -n $currentdate | cut -c -6)
+
 mkdir static/$currentdate 2> /dev/null
+mkdir static/$currentmonth 2> /dev/null
 
 USECACHE=1 RESETCACHE=1 php index.php $currentdate metros > static/$currentdate/metros.html.tmp
 USECACHE=1 php index.php $currentdate trains > static/$currentdate/trains.html.tmp
@@ -16,12 +19,19 @@ USECACHE=1 php json.php $currentdate > static/$currentdate.json.tmp
 USECACHE=1 php csvincidents.php $currentdate > static/$currentdate/incidents.csv.tmp
 USECACHE=1 php csvtimeline.php $currentdate > static/$currentdate/timeline.csv.tmp
 
+php month.php $currentmonth metros > static/$currentmonth/metros.html.tmp
+php month.php $currentmonth trains > static/$currentmonth/trains.html.tmp
+php month.php $currentmonth tramways > static/$currentmonth/tramways.html.tmp
+
 mv static/$currentdate.json{.tmp,}
 mv static/$currentdate/metros.html{.tmp,}
 mv static/$currentdate/trains.html{.tmp,}
 mv static/$currentdate/tramways.html{.tmp,}
 mv static/$currentdate/incidents.csv{.tmp,}
 mv static/$currentdate/timeline.csv{.tmp,}
+mv static/$currentmonth/metros.html{.tmp,}
+mv static/$currentmonth/trains.html{.tmp,}
+mv static/$currentmonth/tramways.html{.tmp,}
 
 ln -fs $todaydate/metros.html static/metros.html
 ln -fs $todaydate/tramways.html static/tramways.html
