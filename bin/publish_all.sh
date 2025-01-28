74464d278bbfe1ba20ec81516ea5de1f75a67449
Vincent LAURENT <vince.laurent@gmail.com>
2025-01-28 08:30:56 +0100

    Export des global vers le dossier datas/export

--- bin/publish_all.sh
+++ bin/publish_all.sh
@@ -2,10 +2,10 @@
 
 find static/[0-9]* -type d | cut -d "/" -f 2 | sort -r | while read file; do bash bin/publish.sh $file 1; done
 
-mkdir static/export 2> /dev/null
+mkdir data/export 2> /dev/null
 
-cat static/*/incidents.csv | head -n 1 > static/export/historique_incidents.csv
-cat static/20*/incidents.csv | grep -v "^date" >> static/export/historique_incidents.csv
+cat static/*/incidents.csv | head -n 1 > data/export/historique_incidents.csv
+cat static/20*/incidents.csv | grep -v "^date" >> data/export/historique_incidents.csv
 
-cat static/*/timeline.csv | head -n 1 > static/export/historique_statuts.csv
-cat static/20*/timeline.csv | grep -v "^date" >> static/export/historique_statuts.csv
+cat static/*/timeline.csv | head -n 1 > data/export/historique_statuts.csv
+cat static/20*/timeline.csv | grep -v "^date" >> data/export/historique_statuts.csv
