c80afddc64abdddb522d5adbe017cbcafc68d66f
Vincent LAURENT <vince.laurent@gmail.com>
2025-01-28 08:50:54 +0100

    La publication du mois se fait au publish_all une fois les exports à jour

--- bin/publish_all.sh
+++ bin/publish_all.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-find static/[0-9]* -type d | cut -d "/" -f 2 | sort -r | while read file; do bash bin/publish.sh $file 1; done
+find static/[0-9]* -type d | grep -E "static/[0-9]{8}" | cut -d "/" -f 2 | sort -r | while read day; do bash bin/publish.sh $day 1; done
 
 mkdir data/export 2> /dev/null
 
-cat static/*/incidents.csv | head -n 1 > data/export/historique_incidents.csv
+cat static/20*/incidents.csv | head -n 1 > data/export/historique_incidents.csv
 cat static/20*/incidents.csv | grep -v "^date" >> data/export/historique_incidents.csv
 
-cat static/*/timeline.csv | head -n 1 > data/export/historique_statuts.csv
+cat static/20*/timeline.csv | head -n 1 > data/export/historique_statuts.csv
 cat static/20*/timeline.csv | grep -v "^date" >> data/export/historique_statuts.csv
+
+find static/[0-9]* -type d | grep -E "static/[0-9]{8}" | cut -d "/" -f 2 | cut -c -6 | sort -r | while read month; do
+    mkdir static/$month 2> /dev/null
+    php month.php $month metros > static/$month/metros.html.tmp
+    php month.php $month trains > static/$month/trains.html.tmp
+    php month.php $month tramways > static/$month/tramways.html.tmp
+    mv static/$month/metros.html{.tmp,}
+    mv static/$month/trains.html{.tmp,}
+    mv static/$month/tramways.html{.tmp,}
+done
