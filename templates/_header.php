e6cb99b216fd0f076c58a90b575b5ede79d55459
Vincent LAURENT <vince.laurent@gmail.com>
2025-03-03 09:24:40 +0100

    Pas le bon chemin

--- templates/_header.php
+++ templates/_header.php
@@ -3,5 +3,5 @@
 <meta name="description" content="Page de suivi et d'historisation de l'état du trafic et des incidents des Métros, RER / Transiliens et Tramways d'Île de France">
 <link rel="icon" href="/images/favicon_<?php echo $mode ?>.ico" />
 <link rel="icon" type="image/png" sizes="192x192" href="/images/favicon_<?php echo $mode ?>.png" />
-<link rel="stylesheet" href="/css/style.css?<?php echo filemtime(__DIR__.'/css/style.css') ?>">
-<script src="/js/main.js?<?php echo filemtime(__DIR__.'/js/main.js') ?>"></script>
+<link rel="stylesheet" href="/css/style.css?<?php echo filemtime(__DIR__.'/../css/style.css') ?>">
+<script src="/js/main.js?<?php echo filemtime(__DIR__.'/../js/main.js') ?>"></script>
