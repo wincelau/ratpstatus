60c65e4349b746aec6d22f5906e5da5a85e2215b
Vincent LAURENT <vince.laurent@gmail.com>
2025-05-02 21:13:36 +0200

    Pas d'emoji finalement

--- templates/_navDate.php
+++ templates/_navDate.php
@@ -1,11 +1,11 @@
 <h1><span class="mobile_hidden">Suivi de l'état du trafic<span> des transports IDF</span></span><span class="mobile_visible">État du trafic</span></h1>
 <h2><a title="Voir le mois précédent" href="<?php echo View::url("/".$period->getDatePrevious()->format($period->getDateFormat())."/".$mode.".html") ?>">⬅️<span class="visually-hidden">Voir la période précédente</span></a>
 <select id="select-day"" onchange="document.location.href=this.value; this.value='';" autocomplete="off">
-    <option style="display: none;" value="" selected="selected"><?php if($period->isToday()):?>📅<?php else: ?>🗓️<?php endif; ?> <?php echo $period->getDateStartLabel(); ?>&nbsp;</option>
+    <option style="display: none;" value="" selected="selected"><?php if($period->isToday()):?>Aujourd'hui<?php else: ?><?php echo $period->getDateStartLabel(); ?><?php endif; ?></option>
     <?php foreach(View::getDatesChoices() as $group => $choices): ?>
     <optgroup label="<?php echo $group ?>">
     <?php foreach($choices as $dateChoiceKey => $dateChoiceLibelle): ?>
-    <option value="<?php echo View::url("/".$dateChoiceKey."/".$mode.".html") ?>"><?php if($period->getDateStartKey() == $dateChoiceKey):?>🔘<?php else: ?>⚪<?php endif; ?> <?php echo $dateChoiceLibelle ?> <?php if($dateChoiceKey == date('Ymd')): ?>📅<?php endif; ?></option>
+    <option value="<?php echo View::url("/".$dateChoiceKey."/".$mode.".html") ?>"><?php if($period->getDateStartKey() == $dateChoiceKey):?>🔘<?php else: ?>⚪<?php endif; ?> <?php echo $dateChoiceLibelle ?><?php if($dateChoiceKey == date('Ymd')): ?> 🔥<?php endif; ?></option>
     <?php endforeach; ?>
     </optgroup>
     <?php endforeach; ?>
