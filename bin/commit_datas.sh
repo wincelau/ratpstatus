bf93a5ec66e122d1054305cb9d115640919cc47e
Vincent LAURENT <vince.laurent@gmail.com>
2025-05-02 08:42:15 +0200

    Commit des données plus précis

--- bin/commit_datas.sh
+++ bin/commit_datas.sh
@@ -4,7 +4,8 @@ commitdate=$(date +%Y%m%d --date="-1 day")
 
 git sparse-checkout set --no-cone '/*' '!/datas/json/' "/datas/json/$commitdate"
 
-git add datas
+git add datas/disruptions_ids
+git add datas/jsonlines
 git add datas/json/$commitdate
 git commit datas -m "Données de la journée $commitdate"
 git pull --commit
