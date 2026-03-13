bb1fe210699aed312e886e947995ca0470fb5373
Vincent LAURENT <vince.laurent@gmail.com>
2026-03-14 00:03:42 +0100

    Exclusion d'incident qui sont des infos

--- app/Impact.php
+++ app/Impact.php
@@ -264,10 +264,10 @@ class Impact
             return self::TYPE_AUCUNE;
         }
 
-        if(preg_match("/Les horaires de départ ou de passage en gare de certains trains peuvent être avancés/", $this->getMessagePlainText())) {
+        if(preg_match("/Les horaires de départ ou de passage en gare de certains trains (de la ligne ? |)peuvent être avancés/", $this->getMessagePlainText())) {
             return self::TYPE_AUCUNE;
         }
-        
+
         if($this->getTitle() == "test") {
 
             return self::TYPE_AUCUNE;
